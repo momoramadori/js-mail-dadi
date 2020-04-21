c4994fe5f941b0e6f19a5ca7046cc887064851de
Momo Ramadori <momoramadori8@gmail.com>
2020-04-21 15:26:30 +0200

    evoluzioni

--- dadi/main.js
+++ dadi/main.js
@@ -1,18 +1,20 @@
 // Chiedere un numero tra 1 e 6 all'utente
-var numeroUtente = prompt("inserisci un numero tra 1 e 6");
+var numeroUtente = parseInt(prompt("inserisci un numero tra 1 e 6"));
 console.log(numeroUtente);
 
 //Generare un numero random con il computer tra 1 e 6
 var min = 1;
 var max = 6;
 function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+  return Math.floor(Math.random() * (6 - 1 + 1 ) ) + 1;
 }
 
 console.log(getRndInteger());
 
-if () {
-
-} else {
-
+if (getRndInteger() > numeroUtente) {
+    console.log("Ha vinto il Computer!");
+} else if (getRndInteger() == numeroUtente ) {
+    console.log("Parità Assoluta!");
+} else if (getRndInteger() < numeroUtente ) {
+    console.log("Complimenti, hai vinto!");
 }
