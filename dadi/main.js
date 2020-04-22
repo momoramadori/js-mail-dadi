e86b96926abcd8d22bc25e1ec6e5b6a4aca15e1f
Momo Ramadori <momoramadori8@gmail.com>
2020-04-22 12:20:53 +0200

    aggiustati dadi

--- dadi/main.js
+++ dadi/main.js
@@ -1,16 +1,20 @@
+
 // Chiedere un numero tra 1 e 6 all'utente
 var numeroUtente = parseInt(prompt("inserisci un numero tra 1 e 6"));
 console.log(numeroUtente);
 
-//Generare un numero random con il computer tra 1 e 6
-var numeroComputer = Math.floor(Math.random() * 6) + 1;
-console.log(numeroComputer);
-
 //Vedere chi ha vinto e visualizzare il messaggio corrispondente
-if ( numeroUtente > numeroComputer ) {
-    console.log('Complimenti, hai vinto!');
-} else if (numeroUtente == numeroComputer) {
-    console.log('Parità!');
+if (isNaN(numeroUtente) == true || numeroUtente < 1 || numeroUtente > 6) {
+    console.log('hai dato un valore errato');
 } else {
-    console.log('Hai perso, ritenta e sarai più fortunato!')
+    //Generare un numero random con il computer tra 1 e 6
+    var numeroComputer = Math.floor(Math.random() * 6) + 1;
+    console.log(numeroComputer);
+    if ( numeroUtente > numeroComputer ) {
+        console.log('Complimenti, hai vinto!');
+    } else if (numeroUtente == numeroComputer) {
+        console.log('Parità!');
+    } else {
+        console.log('Hai perso, ritenta e sarai più fortunato!')
+    }
 }
